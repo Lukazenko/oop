6ccf38438a87af8f2b361783ac80bdd0201b1991
Lukazenko <veikko.kiis@khk.ee>
2018-01-19 09:50:43 +0200

    Kaustade konstandid määratud

---
+++ conf.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 1/19/2018
+ * Time: 9:45 AM
+ */
+
+// kaustade ja failide konstantsed nimetused
+
+define('MODEL_DIR', 'model/');
+define('VIEW_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
+
+
