aa21327c37ee2dec54174c388a8a4638b18ea043
Lukazenko <veikko.kiis@khk.ee>
2018-01-19 10:30:07 +0200

    Muutujad on määratud

---
+++ model/template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 1/19/2018
+ * Time: 10:21 AM
+ */
+
+class template
+{
+    // klassi omadused
+
+    var $file = ''; // HTML malli faili nimi
+    var $content = false; // HTML malli failist loetud sisu
+    var $vars = array(); // HTML malli elementide ja reaalväärtuste paarid
+
+
+}
