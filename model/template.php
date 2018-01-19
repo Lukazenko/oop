494ea9cd7fea30570431a6920d13237be016b38a
Lukazenko <veikko.kiis@khk.ee>
2018-01-19 11:01:41 +0200

    HTML malli laadimine on kirjeldatud

--- model/template.php
+++ model/template.php
@@ -14,14 +14,59 @@ class template
     var $content = false; // HTML malli failist loetud sisu
     var $vars = array(); // HTML malli elementide ja reaalväärtuste paarid
 
-    // HTML malli failist sisu lugemine
+
+    // HTML malli faili nime ja õiguste kontroll ning sisu lugemine, kui tingimused on täidetud
+
+    function loadFile(){
+        if(!is_dir(VIEW_DIR)){
+            echo 'Ei ole leitud '.VIEW_DIR.' kataloogi<br />';
+            exit;
+        }
+
+
+        // kui faili nimi on määratud kujul views/failinimi.html
+
+        $file = $this->file; //abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
+        }
+
+
+
+        // kui faili nimi on määratud kujul failinimi
+
+        $file = VIEW_DIR.$this->file.'.html'; //abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
+        }
+
+        // kui fail asub alamkataloogis views/alamkaust/failinimi.html
+
+
+        $file = VIEW_DIR.str_replace('.','/', $this->file)->file.'.html'; //abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
+        }
+
+        // kui ikkagi sisu on puudu
+
+        if($this->content === false){
+            echo 'Ei suutnud lugeda '.$this->file.' sisu <br />';
+            exit;
+        }
+
+    }
+
+
+
+    // HTML malli failist sisu lugemise funtkstioon
 
     function readFile($file){
         $fp = fopen($file, 'r');
         $this->content = fread($fp, filesize($file));
         fclose($fp);
 
-        // või teine variant:
+        // või teine variant, mis on ühe reaga:
 
         // $this->content = file_get_contents($file);
 
