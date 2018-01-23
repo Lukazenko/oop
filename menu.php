21a9928909fed477a9d7380d73e723315d9ef6d3
Lukazenko <veikko.kiis@khk.ee>
2018-01-23 11:06:52 +0200

    Menüü objekt ja menüü elemendi objekt on loodud

---
+++ menu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 1/23/2018
+ * Time: 11:04 AM
+ */
+
+$menuTmbl = new template('menu.menu');
+
+// loome menüü elemendi malli objekti
+
+$menuItemTmpl = new template('menu.menu_item');
