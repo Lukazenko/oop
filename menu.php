9d1f5de7339b0bd06f0e94ca154a4f2a38ded135
Lukazenko <veikko.kiis@khk.ee>
2018-02-02 11:00:14 +0200

    mysql.php loomine ja menu.php lisasime Avalehe lingi

--- menu.php
+++ menu.php
@@ -13,10 +13,31 @@ $menuTmpl = new template('menu.menu');
 
 $menuItemTmpl = new template('menu.menu_item');
 
+// lisame avalehe
+
+$menuItemTmpl->set('menu_item_name','Avaleht');
+
+
+// loome lingi
+
+$link = $http->getLink();
+$menuItemTmpl->set('link',$link);
+
+
+// täidame loodud elemndiga lehe menüü
+
+$menuItem = $menuItemTmpl->parse();
+$menuTmpl->add('menu_items', $menuItem);
+
+
+// ********************************************************
 // tegutseme ühe menüü elemendiga
 
 $menuItemTmpl->set('menu_item_name','esimene');
+
+
 // loome lingi
+
 $link = $http->getLink(array('control' => 'esimene'));
 $menuItemTmpl->set('link',$link);
 
@@ -26,9 +47,12 @@ $menuItemTmpl->set('link',$link);
 $menuItem = $menuItemTmpl->parse();
 $menuTmpl->add('menu_items', $menuItem);
 
+//*********************************************************
 
 $menuItemTmpl->set('menu_item_name','teine');
+
 // loome lingi
+
 $link = $http->getLink(array('control' => 'teine'));
 $menuItemTmpl->set('link',$link);
 
@@ -37,9 +61,13 @@ $menuItemTmpl->set('link',$link);
 $menuItem = $menuItemTmpl->parse();
 $menuTmpl->add('menu_items', $menuItem);
 
+//************************************************************
 
+
+//***************************************************
 
 // koostame valmis menüü vaate
 $menu = $menuTmpl->parse();
 // ja lisame antud vaate peamalli elemendile nimega {menu}
 $mainTmpl->set('menu', $menu);
+
---
+++ model/mysql.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asd
+ * Date: 2/2/2018
+ * Time: 10:50 AM
+ */
+
+class mysql
+{
+    var $conn = false;  // ühendus andmebaasi serveriga
+    var $host = false;  // serveri nimi IP
+    var $user = false;  // kasutajanimi
+    var $pass = false;  // kasutaja parool
+    var $dbname = false;  // andmebaasi nimi
+
+    /**
+     * mysql constructor.
+     * @param bool $host
+     * @param bool $user
+     * @param bool $pass
+     * @param bool $dbname
+     */
+    public function __construct($host, $user, $pass, $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+        $this->connect(); //ühenduse loomine
+    }
+
+    // funktsioon ühenduseks andmebaasi serveriga
+
+    function connect(){
+        $this->conn = mysqli_connect($this->host, $this->user, $this -> pass, $this->dbname);
+        if($this->conn == false){
+            echo 'Probleem ühendusega!<br />';
+            exit;
+        }
+    }
+
+
+
+}
