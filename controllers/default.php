f884a51cd1c66c16e036948fe966631105dec9f7
Lukazenko <veikko.kiis@khk.ee>
2018-02-06 11:01:58 +0200

    koostati vaikimisi kontrolleri andmebaasi andmete töötlust

--- controllers/default.php
+++ controllers/default.php
@@ -10,5 +10,29 @@
 $mainTmpl->set('content', 'Lehe vaikimisi sisu');
 
 
-$page_id = $http->get('page_id');
-echo getty
+$page_id = (int)$http->get('page_id');
+
+
+// lehe id järgi küsime sisu andmebaasist
+
+$sql = 'SELECT content FROM content'.
+    ' WHERE content_id='.fixDb($page_id);
+
+// küsime vajalikud andmed andmebaasist
+$result = $db->getData($sql);
+
+// kui vaatavale page_id-le ei leidu andmebaasis vastust
+
+if($result == false) {
+    //siis pöördume avalehele first page
+    $sql = 'SELECT * FROM content WHERE ' .
+        'is_first_page=' . fixDb(1);
+    $result = $db->getData($sql);
+}
+
+if($result != false){
+    //siis tulemus koosneb ainult ühest reast ja see ongi vastava lehe sisu
+    $page = $result[0];
+    $mainTmpl->set('content', $page['content']);
+}
+
