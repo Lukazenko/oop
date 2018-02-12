abb50b30b058e137d6236f339c3d2ad0523e3b7a
Lukazenko <veikko.kiis@khk.ee>
2018-02-12 09:50:28 +0200

    lingitud nupp mis on sisselogimiseks

--- controllers/login.php
+++ controllers/login.php
@@ -16,6 +16,11 @@ $loginForm->set('kasutaja', 'Kasutajanimi');
 $loginForm->set('parool', 'Kasutaja parool');
 $loginForm->set('nupp', 'Logi sisse!');
 
+//loome lingi vormi töötluseks
+
+$link = $http->getLink(array('control'=>'login_do'));
+$loginForm->set('link', $link);
+
 // paneme väärtused malli
 // selleks oleks vaja trükkida sisse logimisvorm {content} elemendis
 
